63bb49ce11afc1cbb73ad9647be3902dd83d7419
irvin_cardozo <irvin_cardozo@hacienda.gov.py>
2025-02-15 13:02:24 -0300

    [fix] microdot se instala localmente mediante archivo y se cambia boot

--- boot.py
+++ boot.py
@@ -1,34 +1,14 @@
 # boot.py
 import network
-import webrepl
 
 def setup_network():
-    # Configurar WiFi
     sta_if = network.WLAN(network.STA_IF)
     sta_if.active(True)
     sta_if.connect('tu_ssid', 'tu_password')
-
-    # Esperar conexión
+    
     while not sta_if.isconnected():
         pass
     print('Conectado a WiFi:', sta_if.ifconfig())
 
-def main():
-    try:
-        setup_network()
-        # Iniciar WebREPL directamente sin setup
-        webrepl.start(password='tupassword')  # Define una contraseña directamente
-        print('WebREPL iniciado')
-        
-        # Continuar con la instalación de paquetes
-        import upip
-        print('Instalando paquetes...')
-        upip.install('microdot')
-        upip.install('micropython-uplot')
-        print('Paquetes instalados')
-        
-    except Exception as e:
-        print('Error en setup:', e)
-
 if __name__ == '__main__':
-    main()
+    setup_network()
